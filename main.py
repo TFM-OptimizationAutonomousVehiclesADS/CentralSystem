8c3c92536efbc45430b690223de0fcb57213d220
jesuscumpli <jesuscumpli@uma.es>
2023-03-18 00:05:44 +0100

    api service

--- main.py
+++ main.py
@@ -13,6 +13,11 @@ app = FastAPI(middleware=[
 ])
 dockerClient = docker.from_env()
 
+image_digital_model_name = "jesuscumpli/digital-model"
+image_digital_model_tag = "mdv2"
+image_real_sytem_name = "jesuscumpli/real-system"
+image_real_system_tag = "rsv2"
+
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
@@ -66,8 +71,7 @@ async def all_images():
 
 @app.get("/digital-models/all")
 async def all_digital_models():
-    image_name = "mdv2"
-    containers = dockerClient.containers.list(all=True, filters={"ancestor": image_name})
+    containers = dockerClient.containers.list(all=True, filters={"ancestor": f"{image_digital_model_name}:{image_digital_model_tag}"})
     digital_models = []
     for container in containers:
         data = {}
@@ -83,8 +87,7 @@ async def all_digital_models():
 
 @app.get("/all_real_systems")
 async def all_real_systems():
-    image_name = "rsv2"
-    containers = dockerClient.containers.list(all=True, filters={"ancestor": image_name})
+    containers = dockerClient.containers.list(all=True, filters={"ancestor": f"{image_real_sytem_name}:{image_real_system_tag}"})
     real_systems = []
     for container in containers:
         data = {}
@@ -100,10 +103,10 @@ async def all_real_systems():
 
 @app.get("/digital-models/new")
 async def digital_models_new():
-    image_name = "mdv2"
     container_name = "mdv2-" + str(uuid.uuid1())
+    dockerClient.images.pull(image=f"{image_digital_model_name}:{image_digital_model_tag}")
     options = {
-        "image": image_name,
+        "image": f"{image_digital_model_name}:{image_digital_model_tag}",
         "name": container_name,
         "detach": True,  # Ejecutar el contenedor en segundo plano
         "ports": {"8001/tcp": None}
