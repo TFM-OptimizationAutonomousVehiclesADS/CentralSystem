5d3d03c26fb721255bb28ba3c678facf2d2a7fcf
Jesus Cumplido <jesus.cumplido@ravenloop.io>
2023-04-28 20:48:50 +0200

    update central system

--- main.py
+++ main.py
@@ -386,8 +386,6 @@ async def real_system_replace_model(id_container: str):
 
         print("QUERY: REPLACE ACTUAL MODEL")
         query_post_replace_model = "/replace_actual_model"
-        headers = {"Content-Type": "multipart/form-data"}
-        model_bytes = b"contenido del archivo"
         response = requests.post(f"http://127.0.0.1:8081{query_post_replace_model}", files={"model_bytes": model_bytes},
                                  data={"evaluation_dict": json.dumps(evaluation_dict)},  timeout=20)
         success = response.json()
